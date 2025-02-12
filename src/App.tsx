36da017684aa23752b9379e14934d3eec8f49e2d
matthew-burtenshaw <matthew.d.burtenshaw@gmail.com>
2025-02-12 14:51:33 +0000

    Removing unneeded assets and react-native-edge-to-edge library

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import { Assets as NavigationAssets } from '@react-navigation/elements';
-import { Asset } from 'expo-asset';
 import * as SplashScreen from 'expo-splash-screen';
 import * as React from 'react';
 import { Navigation } from './navigation';
 import { database } from './model/database';
 import { DatabaseProvider } from '@nozbe/watermelondb/react';
-
-Asset.loadAsync([
-  ...NavigationAssets,
-  require('./assets/newspaper.png'),
-  require('./assets/bell.png'),
-]);
 
 SplashScreen.preventAutoHideAsync();
 
